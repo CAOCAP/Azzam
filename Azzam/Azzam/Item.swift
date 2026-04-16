eb8004ef4948651649cc15d28e884febf070a0b7
azzam-dev <azzam.RAR@gmail.com>
2026-04-16 21:13:33 +0300

    chore: add .DS_Store to gitignore and initial project setup

---
+++ Azzam/Azzam/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Azzam
+//
+//  Created by الشيخ عزام on 16/04/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Azzam/AzzamTests/AzzamTests.swift
@@ -0,0 +1,16 @@
+//
+//  AzzamTests.swift
+//  AzzamTests
+//
+//  Created by الشيخ عزام on 16/04/2026.
+//
+
+import Testing
+
+struct AzzamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
